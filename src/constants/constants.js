444555846dc146a867796a8f765d9bb40f6e1ab7
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-04-23 12:54:04 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-const API_KEY = "AIzaSyBdRcielsjxDkP5mWf17XtfMtrFMs9PL8I";
+const API_KEY = "[YOUR_API_KEY]";
 
-const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY;
+const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=[YOUR_API_KEY]";
 
 export default YOUTUBE_API_URL;
